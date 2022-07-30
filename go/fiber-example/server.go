54f23ea13a42a09974d15a51df4df40e98475766
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-07-30 14:12:55 +0800

    :heavy_plus_sign: Added `gofiber` example

---
+++ go/fiber-example/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "fmt"
+  "log"
+
+  "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+  server := fiber.New()
+
+  server.Get("/", Greet)
+
+  log.Fatal(server.Listen(":8080"))
+}
+
+func Greet(ctx *fiber.Ctx) error {
+  name := ctx.Query("name", "World") // /?name={name} -> Hello, {name}!
+  greeting := fmt.Sprintf("Hello, %s!\n", name)
+
+  return ctx.Status(200).SendString(greeting)
+}
+
