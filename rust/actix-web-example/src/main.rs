cf10c7883c1120051a4398ffb7f29cddd2ce8b74
Idontknow13 <pokeminecraftexpert@gmail.com>
2022-07-31 13:40:18 +0800

    :pencil2: Added logging to `Actix-Web`

--- rust/actix-web-example/src/main.rs
+++ rust/actix-web-example/src/main.rs
@@ -1,15 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
-
-type IOResult<T> = std::io::Result<T>;
 
 const LOCALHOST: (&str, u16) = ("127.0.0.1", 8080);
 
+type IOResult<T> = std::io::Result<T>;
+
 #[actix_web::main]
 async fn main() -> IOResult<()> {
+    // Enable logging
+    use env_logger::Env;
+    env_logger::init_from_env(Env::new().default_filter_or("info"));
+
     println!("Running server at {}:{}...", LOCALHOST.0, LOCALHOST.1);
 
-    HttpServer::new(|| App::new().service(greet))
+    // View logging semantics [here](https://actix.rs/docs/middleware/).
+    HttpServer::new(|| App::new().wrap(Logger::default()).service(greet))
         .bind(LOCALHOST)?
         .run()
         .await
